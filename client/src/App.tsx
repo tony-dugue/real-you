761d43af8ad015f1f2de878583a5858fb40cbd5e
tony dugué <duguetony@gmail.com>
2023-01-31 00:46:51 +0100

    client => add custom MUI theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,32 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useMemo } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from './scenes/homePage';
-import LoginPage from './scenes/loginPage';
-import ProfilePage from './scenes/profilePage';
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { useAppSelector } from "./hooks/custom-redux-hooks";
 
 const App: FunctionComponent = () => {
+  const mode = useAppSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/home' element={<HomePage />} />
-          <Route path='/profile/:userId' element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
-  )
+  );
 };
 
 export default App;
---
+++ client/src/interfaces/emotion.d.ts
@@ -0,0 +1,40 @@
+import "@emotion/react";
+
+declare module "@emotion/react" {
+  export interface Theme {
+    palette: {
+      mode: string;
+      primary: any;
+      neutral: any;
+      background: any;
+    };
+    typography: {
+      fontFamily: string[];
+      fontSize: number;
+      h1: {
+        fontFamily: string[];
+        fontSize: number;
+      };
+      h2: {
+        fontFamily: string[];
+        fontSize: number;
+      };
+      h3: {
+        fontFamily: string[];
+        fontSize: number;
+      };
+      h4: {
+        fontFamily: string[];
+        fontSize: number;
+      };
+      h5: {
+        fontFamily: string[];
+        fontSize: number;
+      };
+      h6: {
+        fontFamily: string[];
+        fontSize: number;
+      };
+    };
+  }
+}
